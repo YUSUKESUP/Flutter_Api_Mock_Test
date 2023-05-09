81ecf3f2b3d17b2c61aad7e9d5080fcaa54d20bf
YUUSKE <yusuke.1173ab@gmail.com>
2023-05-09 13:59:32 +0900

    APIの取得方法をDioに変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,17 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_api_mock_test/screen/home_screen.dart';
 
-const maxNumber = 385;
-
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(
-        title: 'わかりやすい',
-        key: ValueKey('myHomePageKey'),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late List<Map<String, dynamic>> pokemonDataList;
-  late List<String> imageUrls;
-
-  @override
-  void initState() {
-    pokemonDataList = [];
-    imageUrls = [];
-    fetchPokemonData();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: GridView.builder(
-        itemCount: pokemonDataList.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-        ),
-        itemBuilder: (context, int index) {
-          final pokemonData = pokemonDataList[index];
-          return SizedBox(
-            height: 120,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 100,
-                  child: CachedNetworkImage(
-                    imageUrl: imageUrls[index],
-                    placeholder: (context, url) => Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                    errorWidget: (context, url, error) => Icon(Icons.error),
-                  ),
-                ),
-                Text(
-                  '#${pokemonData['id']} ${pokemonData['name']}',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  Future<void> fetchPokemonData() async {
-    for (int index = 1; index <= maxNumber; index++) {
-      final url = 'http://pokeapi.co/api/v2/pokemon/$index/';
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        var jsonResponse = json.decode(response.body);
-        final imageUrl = jsonResponse['sprites']['front_default'];
-        final pokemonData = {
-          'id': jsonResponse['id'],
-          'name': jsonResponse['name'],
-        };
-        setState(() {
-          imageUrls.add(imageUrl);
-          pokemonDataList.add(pokemonData);
-        });
-      } else {
-        throw Exception('Failed to fetch Pokemon data');
-      }
-    }
-  }
-}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,79 +1,100 @@
 import 'dart:convert';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-import '../model/model.dart';
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  var marvelApiUrl = "https://mcuapi.herokuapp.com/api/v1/movies";
-  List<MCUDATA?> mcuMoviesList = [];
+class _MyHomePageState extends State<MyHomePage> {
+  late List<Map<String, dynamic>> pokemonDataList;
+  late List<String> imageUrls;
 
+  static const maxNumber = 385;
 
+  final Dio _dio = Dio(BaseOptions(baseUrl: 'http://pokeapi.co/api/v2/'));
 
   @override
   void initState() {
-    // TODO: implement initState
+    pokemonDataList = [];
+    imageUrls = [];
+    fetchPokemonData();
     super.initState();
-    getMarvelMovies();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[900],
-        body: mcuMoviesList.isNotEmpty
-            ? GridView.builder(
-          itemCount: mcuMoviesList.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 2 / 3,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10),
-          itemBuilder: (BuildContext context, int index) {
-            return ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: mcuMoviesList[index]!.coverUrl != null
-                    ? CachedNetworkImage(
-                  imageUrl:
-                  mcuMoviesList[index]!.coverUrl.toString(),
-                  placeholder: (context, url) =>
-                      Image.asset('images/place_holder.png'),
-                )
-                    : Image.asset('images/place_holder.png'));
-          },
-        )
-            : Center(
-            child: Container(
-              width: 50,
-              height: 50,
-              child: Center(
-                child: CircularProgressIndicator(
-                  color: Colors.white70,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: const Text(
+          'ポケモン',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: GridView.builder(
+        itemCount: pokemonDataList.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemBuilder: (context, int index) {
+          final pokemonData = pokemonDataList[index];
+          return SizedBox(
+            height: 120,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 100,
+                  child: CachedNetworkImage(
+                    imageUrl: imageUrls[index],
+                    placeholder: (context, url) => const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                    errorWidget: (context, url, error) =>
+                    const Icon(Icons.error),
+                  ),
                 ),
-              ),
-            )));
+                Text(
+                  '#${pokemonData['id']} ${pokemonData['name']}',
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 
-  void getMarvelMovies() {
-    final uri = Uri.parse(marvelApiUrl);
-    http.get(uri).then((response) {
-      if (response.statusCode == 200) {
-        final responseBody = response.body;
-        final decodedData = jsonDecode(responseBody);
-        mcuMoviesList = mcg_model.fromJson(decodedData).data as List<MCUDATA?>;
-        print(mcuMoviesList.first!.id);
-        setState(() {});
-      } else {}
-    }).catchError((err) {
-      debugPrint('=========== $err =============');
-    });
+  Future<void> fetchPokemonData() async {
+    for (int index = 1; index <= maxNumber; index++) {
+      try {
+        final Response<dynamic> response =
+        await _dio.get('pokemon/$index/');
+        if (response.statusCode == 200) {
+          var jsonResponse = response.data;
+          final imageUrl = jsonResponse['sprites']['front_default'];
+          final pokemonData = {
+            'id': jsonResponse['id'],
+            'name': jsonResponse['name'],
+          };
+          setState(() {
+            imageUrls.add(imageUrl);
+            pokemonDataList.add(pokemonData);
+          });
+        } else {
+          throw Exception('Failed to fetch Pokemon data');
+        }
+      } catch (e) {
+        throw Exception('Failed to fetch Pokemon data');
+      }
+    }
   }
 }
